b812d144c75e737dfc526be5e2496c593a10d870
MiklosArpad <43209274+MiklosArpad@users.noreply.github.com>
2022-02-13 14:04:22 +0100

    Add mock endpoint (#3)

---
+++ src/main/java/hu/ingatlancentrum/api/controller/MessageController.java
@@ -0,0 +1,32 @@
+package hu.ingatlancentrum.api.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hu.ingatlancentrum.api.model.Message;
+
+@RestController
+@RequestMapping("/api/v1/messages")
+public class MessageController {
+
+    @GetMapping
+    public ResponseEntity<List<Message>> getMessages() {
+        var message1 = new Message();
+        message1.setText("Hello world! #1");
+
+        var message2 = new Message();
+        message2.setText("Hello world! #2");
+
+        var messages = new ArrayList<Message>();
+        messages.add(message1);
+        messages.add(message2);
+
+        return new ResponseEntity<List<Message>>(messages, HttpStatus.OK);
+    }
+}
