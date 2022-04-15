77402a517e4080f5c233c425aab8913ff5205722
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-04-15 12:22:02 +0200

    Fix format

--- src/test/java/hu/ingatlancentrum/api/ApplicationTests.java
+++ src/test/java/hu/ingatlancentrum/api/ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-		// Empty test
-	}
+    @Test
+    void contextLoads() {
+        // Empty test
+    }
 }
