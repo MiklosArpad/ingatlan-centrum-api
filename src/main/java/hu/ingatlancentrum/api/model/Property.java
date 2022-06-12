3ac6b826569cac1b2425c1afe36626b878c25c32
MiklosArpad <miklosarpad2007@gmail.com>
2022-06-12 12:02:51 +0200

    Add JPA annotations to Property model

--- src/main/java/hu/ingatlancentrum/api/model/Property.java
+++ src/main/java/hu/ingatlancentrum/api/model/Property.java
@@ -1,6 +1,17 @@
 package hu.ingatlancentrum.api.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "properties")
 public class Property {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String address;
 }
