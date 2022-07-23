77f8d082f966149bbca9ab4e0aedd37cec79e218
MiklosArpad <miklosarpad2007@gmail.com>
2022-07-23 17:36:39 +0200

    Add getters and setters to Property

--- src/main/java/hu/ingatlancentrum/api/model/Property.java
+++ src/main/java/hu/ingatlancentrum/api/model/Property.java
@@ -14,4 +14,20 @@ public class Property {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
