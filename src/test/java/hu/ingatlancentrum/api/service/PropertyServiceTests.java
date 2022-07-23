1f1469f5999ff87c3cafd52aeaa52d6b025516b6
MiklosArpad <miklosarpad2007@gmail.com>
2022-07-23 17:36:56 +0200

    Add unit test for get all properties

--- src/test/java/hu/ingatlancentrum/api/service/PropertyServiceTests.java
+++ src/test/java/hu/ingatlancentrum/api/service/PropertyServiceTests.java
@@ -1,2 +1,34 @@
-package hu.ingatlancentrum.api.service;public class PropertyServiceTests {
+package hu.ingatlancentrum.api.service;
+
+import hu.ingatlancentrum.api.model.Property;
+import hu.ingatlancentrum.api.repository.PropertyRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class PropertyServiceTests {
+    @Mock
+    private PropertyRepository propertyRepository;
+    @InjectMocks
+    private PropertyService propertyService;
+
+    @Test
+    public void getPropertiesTest() {
+        var property = new Property(); // TODO: create test builders
+        property.setId(1L);
+        property.setAddress("Szeged");
+
+        when(propertyRepository.findAll())
+                .thenReturn(List.of(property));
+
+        var properties = propertyService.getProperties();
+        assertEquals(1, properties.size());
+    }
 }
