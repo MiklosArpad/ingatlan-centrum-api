2ebefa34976f33bfd42c51fafb1e68ce3c9626c7
MiklosArpad <miklosarpad2007@gmail.com>
2022-08-13 16:09:46 +0200

    Implement remaining endpoints.

--- src/main/java/hu/ingatlancentrum/api/controller/PropertyController.java
+++ src/main/java/hu/ingatlancentrum/api/controller/PropertyController.java
@@ -1,11 +1,15 @@
 package hu.ingatlancentrum.api.controller;
 
 import hu.ingatlancentrum.api.constants.RouteConstants;
+import hu.ingatlancentrum.api.exception.PropertyNotFoundException;
 import hu.ingatlancentrum.api.model.Property;
 import hu.ingatlancentrum.api.service.PropertyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +28,23 @@ public class PropertyController {
     public ResponseEntity<List<Property>> getProperties() {
         var properties = propertyService.getProperties();
         return new ResponseEntity<>(properties, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Property> getProperty(@PathVariable Long id) {
+        try {
+            var property = propertyService.getProperty(id);
+            return new ResponseEntity<>(property, HttpStatus.OK);
+        } catch (PropertyNotFoundException propertyNotFoundException) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception exception) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Property> createProperty(@RequestBody Property property) {
+        var createdProperty = propertyService.addProperty(property);
+        return new ResponseEntity<>(createdProperty, HttpStatus.CREATED);
     }
 }
