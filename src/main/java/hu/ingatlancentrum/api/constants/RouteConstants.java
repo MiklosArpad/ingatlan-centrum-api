f7eb5d1b71d9c2be9f23984b6881460102d0901e
MiklosArpad <43209274+MiklosArpad@users.noreply.github.com>
2022-08-21 15:57:13 +0200

    [ICA-1] - Implement 'Properties' endpoints (#1)
    * Add empty Property model
    
    * Install Sping Data and MySQL connector for Java
    
    * Configure database
    
    * Add comment
    
    * Fix format
    
    * Remove database connection details from application.yml
    
    * Temp switch off JPA Data Source Spring autoconfiguration
    
    * Add get all properties flow
    
    * Create repository, service and controller components
    
    * Remove unused code
    
    * Add component annotations
    
    * Fine-tune YAML files
    
    * Rename file
    
    * Change to 'PostgreSQL' dependency
    
    * Add JPA annotations to Property model
    
    * Change to PosgreSQL configuration
    
    * Revert "Change to 'PostgreSQL' dependency"
    
    This reverts commit a3c4f0cc244c30c58fa20e5dc56c860a321afdc9.
    
    * Move back to MySQL config
    
    * Fix issues in app-dev yaml file
    
    * Move endpoint magic strings to constant values
    
    * Exclude constant file from code coverage
    
    * Empty commit
    
    * Add Mockito dependency
    
    * Add getters and setters to Property
    
    * Add unit test for get all properties
    
    * Implement get property by id and add unit tests
    
    * Implement add property function and add unit test
    
    * Refactor obj. initializations to test builders objects
    
    * Implement remaining endpoints.
    
    * Use the proper database secrets file and turn on ddl-auto JPA prop.
    
    * Move "id" string to constant value
    
    * Add lombok to dependencies
    
    * Clean up boilerplate code from Property model
    
    * Move magic string to constant value
    
    * Clean up some more boilerplate code
    
    Co-authored-by: Bence Juhasz <juhasz.bence.zsolt@gmail.com>

---
+++ src/main/java/hu/ingatlancentrum/api/constants/RouteConstants.java
@@ -0,0 +1,7 @@
+package hu.ingatlancentrum.api.constants;
+
+public class RouteConstants {
+    private static final String API = "/api/";
+    public static final String PROPERTIES = API + "properties";
+    public static final String ID = "/{id}/";
+}
---
+++ src/main/java/hu/ingatlancentrum/api/controller/PropertyController.java
@@ -0,0 +1,48 @@
+package hu.ingatlancentrum.api.controller;
+
+import hu.ingatlancentrum.api.constants.RouteConstants;
+import hu.ingatlancentrum.api.exception.PropertyNotFoundException;
+import hu.ingatlancentrum.api.model.Property;
+import hu.ingatlancentrum.api.service.PropertyService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(RouteConstants.PROPERTIES)
+@AllArgsConstructor
+public class PropertyController {
+    private final PropertyService propertyService;
+
+    @GetMapping
+    public ResponseEntity<List<Property>> getProperties() {
+        var properties = propertyService.getProperties();
+        return new ResponseEntity<>(properties, HttpStatus.OK);
+    }
+
+    @GetMapping(value = RouteConstants.ID)
+    public ResponseEntity<Property> getProperty(@PathVariable Long id) {
+        try {
+            var property = propertyService.getProperty(id);
+            return new ResponseEntity<>(property, HttpStatus.OK);
+        } catch (PropertyNotFoundException propertyNotFoundException) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception exception) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Property> createProperty(@RequestBody Property property) {
+        var createdProperty = propertyService.addProperty(property);
+        return new ResponseEntity<>(createdProperty, HttpStatus.CREATED);
+    }
+}
