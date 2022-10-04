376859963fd842293e9bc6f8f7bbe6084e74b4ab
MiklosArpad <miklosarpad2007@gmail.com>
2022-10-04 18:48:47 +0200

    Unit test VendorToVendorResponseMapper

---
+++ src/test/java/hu/ingatlancentrum/api/resource/mapper/request/CreateVendorRequestToVendorMapperTests.java
@@ -0,0 +1,12 @@
+package hu.ingatlancentrum.api.resource.mapper.request;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateVendorRequestToVendorMapperTests {
+
+    @Test
+    void map() {
+    }
+}
